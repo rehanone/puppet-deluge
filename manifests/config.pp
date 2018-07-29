45b097c37bc6232092c2e12df32b3d1ae7b6d9c5
Rehan Mahmood <rehanone@gmail.com>
2018-07-29 17:14:39 -0400

    - Updated `pdk` templates. - Added tests for Ubuntu 18.04 release.

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,55 +2,51 @@ class deluge::config () inherits deluge {
 
   assert_private("Use of private class ${name} by ${caller_module_name}")
 
-  $user = $deluge::service_user
-  $home = $deluge::service_home
-  $service = $deluge::service_server
-
-  if ! defined(User[$user]) {
-    user {'deluge':
+  if !defined(User[$deluge::service_user]) {
+    user { 'deluge':
       ensure => present,
       system => true,
     }
   }
 
-  file { $home:
+  file { $deluge::service_home:
     ensure  => directory,
-    owner   => $user,
-    group   => $user,
+    owner   => $deluge::service_user,
+    group   => $deluge::service_user,
     mode    => '0755',
-    require => User[$user],
+    require => User[$deluge::service_user],
   }
-  -> file { "${home}/.config":
+  -> file { "${deluge::service_home}/.config":
     ensure  => directory,
-    owner   => $user,
-    group   => $user,
+    owner   => $deluge::service_user,
+    group   => $deluge::service_user,
     mode    => '0750',
-    require => User[$user],
+    require => User[$deluge::service_user],
   }
-  -> file { "${home}/.config/deluge":
+  -> file { "${deluge::service_home}/.config/deluge":
     ensure  => directory,
-    owner   => $user,
-    group   => $user,
+    owner   => $deluge::service_user,
+    group   => $deluge::service_user,
     mode    => '0750',
-    require => User[$user],
-    notify  => Service[$service],
+    require => User[$deluge::service_user],
+    notify  => Service[$deluge::service_server],
   }
   -> exec { 'deluge-console-allow-remote':
     command     => "deluge-console \"config -s allow_remote True\"",
     path        => '/usr/bin',
-    subscribe   => File["${home}/.config/deluge"],
+    subscribe   => File["${deluge::service_home}/.config/deluge"],
     refreshonly => true,
-    group       => $user,
-    notify      => Service[$service],
+    group       => $deluge::service_user,
+    notify      => Service[$deluge::service_server],
   }
-  -> concat { "${home}/.config/deluge/auth":
+  -> concat { "${deluge::service_home}/.config/deluge/auth":
     ensure_newline => true,
-    owner          => $user,
-    group          => $user,
+    owner          => $deluge::service_user,
+    group          => $deluge::service_user,
     mode           => '0750',
   }
   concat::fragment { 'deluge_default_user':
-    target  => "${home}/.config/deluge/auth",
+    target  => "${deluge::service_home}/.config/deluge/auth",
     content => 'localclient:690f6fe7c1262f4282e84ab5dee7e8f92eed3855:10',
   }
 
