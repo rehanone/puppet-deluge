535b71c152cabf24c58518b2dbfd2bd425b0227a
Rehan Mahmood <rehanone@gmail.com>
2020-03-26 20:39:44 -0400

    - Added support for service umask to be configurable using `deluge::service_server_umask`. - Added support for service webui umask to be configurable using `deluge::service_webui_umask`. - Updated `pdk` templates.

--- templates/service/systemd/deluged.service.render.sh
+++ templates/service/systemd/deluged.service.render.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-puppet epp render deluge.service.epp --values '{ service_user => deluge }'
+puppet epp render deluged.service.epp --values '{ service_user => deluge, service_umask => "002" }'
